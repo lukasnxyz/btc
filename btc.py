149a6d37edd088f4f3356b2a146962ce34803aa5
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2024-10-02 12:12:16 +0200

    working on implementation, need to better understand the math tho

--- btc.py
+++ btc.py
@@ -65,5 +65,57 @@ def extended_euclidean_algorithm(a, b):
 
 def inv(n, p):
   """ returns modular multiplicate inverse m s.t. (n * m) % p == 1 """
-  gcd, x, y = extended_euclidean_algorithm(n, p) # pylint: disable=unused-variable
+  gcd, x, y = extended_euclidean_algorithm(n, p) 
   return x % p
+
+def elliptic_curve_addition(self, other: Point) -> Point:
+  # handle special case of P + 0 = 0 + P = 0
+  if self == INF: return other
+  if other == INF: return self
+  # handle special case of P + (-P) = 0
+  if self.x == other.x and self.y != other.y: return INF
+  # compute the slope
+  if self.x == other.x:
+    m = (3 * self.x**2 + self.curve.a) * inv(2 * self.y, self.curve.p)
+  else:
+    m = (self.y - other.y) * inv(self.x - other.x, self.curve.p)
+  # compute the new point
+  rx = (m**2 - self.x - other.x) % self.curve.p
+  ry = (-(m*(rx - self.x) + self.y)) % self.curve.p
+  return Point(self.curve, rx, ry)
+Point.__add__ = elliptic_curve_addition
+
+sk = 1 # secret key
+pk = G # public key (because 1 * G = G)
+print(f"secret key: {sk}\npublic key: {(pk.x, pk.y)}")
+print("Verify public key is on the curve: ", (pk.y**2 - pk.x**3 - 7) % bitcoin_curve.p == 0)
+# if it was 2, the public key is G + G:
+sk = 2
+pk = G + G
+print(f" secret key: {sk}\n public key: {(pk.x, pk.y)}")
+print("Verify the public key is on the curve: ", (pk.y**2 - pk.x**3 - 7) % bitcoin_curve.p == 0)
+# etc.:
+sk = 3
+pk = G + G + G
+print(f" secret key: {sk}\n public key: {(pk.x, pk.y)}")
+print("Verify the public key is on the curve: ", (pk.y**2 - pk.x**3 - 7) % bitcoin_curve.p == 0)
+
+def double_and_add(self, k: int) -> Point:
+  assert isinstance(k, int) and k >= 0
+  result = INF
+  append = self
+  while k:
+    if k & 1:
+      result += append
+    append += append
+    k >>= 1
+  return result
+Point.__rmul__ = double_and_add
+
+print(G == 1*G)
+print(G + G == 2*G)
+print(G + G + G == 3*G)
+
+public_key = secret_key * G
+print(f"x: {public_key.x}\ny: {public_key.y}")
+print("Verify the public key is on the curve: ", (public_key.y**2 - public_key.x**3 - 7) % bitcoin_curve.p == 0)
